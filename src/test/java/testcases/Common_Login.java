0aed43836166e8c31174edabe0409d20de283059
Soujanya <Soujanya@IRL-197>
2021-05-13 18:58:01 +0530

    smoke testing

--- src/test/java/testcases/Common_Login.java
+++ src/test/java/testcases/Common_Login.java
@@ -1,44 +1,57 @@
 package testcases;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.spi.LoggerRepository;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
+import com.freemium.base.BaseClass;
 import com.freemium.pageactions.PageActions;
 import com.freemium.pagelocators.PageLocators;
 import com.freemium.utilities.Utilities;
 
-public class Common_Login extends Utilities {
+public class Common_Login extends BaseClass {
 
+	@BeforeTest
+	public void urlSetUp() throws IOException {
+		driver.manage().deleteAllCookies();
+		driver.get(properties.getProperty("freemiumDevUrl"));
+	}
+
+	@Test
 	public void freemiumUserlogin() throws Exception {
 		try {
 			PageActions actions = new PageActions();
 
 			waitState();
-			driver.manage().deleteAllCookies();
+			/*driver.manage().deleteAllCookies();
 			logger.info("Deleted all cookies");
 			hardRefresh();
-			logger.info("Hard refresh");
+			logger.info("Hard refresh");*/
 			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(PageLocators.userId)));
 			actions.userNameParameter(getFreemiumUserName());
 			logger.info("waiting for next button login page in 10 sec");
-			Thread.sleep(10000);
+			// Thread.sleep(10000);
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(PageLocators.nextButtonXpath)));
 			PageActions.clickOnNextButton();
 			logger.info("waiting for password passing sso page in 10 sec");
-			/*hardRefresh();
-			logger.info("Hard refresh");*/
-			Thread.sleep(10000);
-			//driver.manage().deleteAllCookies();
+			/*
+			 * hardRefresh(); logger.info("Hard refresh");
+			 */
+			// Thread.sleep(10000);
+			// driver.manage().deleteAllCookies();
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(PageLocators.passwordId)));
 			PageActions.passwordParameter(getFremiumPassword());
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(PageLocators.signInButtonId)));
 			logger.info("waiting for sign button sso page in 6 sec");
 			Thread.sleep(6000);
-			//driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			// driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 			logger.info("wait completed for sso page");
 			PageActions.clickOnSignInButton();
 			logger.info("clickOnSignInButton");
@@ -47,6 +60,7 @@ public class Common_Login extends Utilities {
 		}
 	}
 
+	@AfterClass
 	public void logoutPage() throws Exception {
 		try {
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(PageLocators.adminIconClassName)));
